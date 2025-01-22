d548687c23e271008703db6fe7cee023c2ef6450
Brett Hardy <brett8hardy@gmail.com>
2025-01-22 01:34:59 -0800

    Added new room, camera changes
    a camera for each room with different rotation value and lerping to each camera. added skybox

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -18,17 +18,23 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (ball.transform.position.x < -5f && ball.transform.position.z > 5f)
-        {
-            transform.position = Vector3.Lerp(transform.position, new Vector3(pos3.position.x, pos3.position.y, pos3.position.z), cameraSpeed * Time.deltaTime);
-        }
-        if (ball.transform.position.x < -5f && ball.transform.position.z < 5f)
-        {
-            transform.position = Vector3.Lerp(transform.position, new Vector3(pos2.position.x, pos2.position.y, pos2.position.z), cameraSpeed * Time.deltaTime);
-        }
+        //Room 1
         if (ball.transform.position.x > -5f && ball.transform.position.z < 5f)
         {
             transform.position = Vector3.Lerp(transform.position, new Vector3(pos1.position.x, pos1.position.y, pos1.position.z), cameraSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, pos1.rotation, cameraSpeed * Time.deltaTime);
+        }
+        //Room 2
+        if (ball.transform.position.x < -5f && ball.transform.position.z < 5f)
+        {
+            transform.position = (Vector3.Lerp(transform.position, new Vector3(pos2.position.x, pos2.position.y, pos2.position.z), cameraSpeed * Time.deltaTime));
+            transform.rotation = Quaternion.Lerp(transform.rotation, pos2.rotation, cameraSpeed * Time.deltaTime);
+        }
+        //Room 3
+        if (ball.transform.position.x < -5f && ball.transform.position.z > 5f)
+        {
+            transform.position = (Vector3.Lerp(transform.position, new Vector3(pos3.position.x, pos3.position.y, pos3.position.z), cameraSpeed * Time.deltaTime));
+            transform.rotation = Quaternion.Lerp(transform.rotation,pos3.rotation,cameraSpeed * Time.deltaTime);
         }
     }
 }
